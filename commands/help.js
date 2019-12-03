eccd6c52b9695d9479008fb740fb737da60611d6
njn-R <njn@protonmail.com>
2019-12-03 21:07:59 +0530

    help function

---
+++ commands/help.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose').set('debug', true);
+const Collection = require("../models/model.js");
+
+module.exports =
+    {
+	    name: 'help',
+	    description: 'Help commands',
+        execute(message)
+        {         
+            
+            message.channel.send(">create 1 abc \n >join 1 \n >leave 1 \n >list \n >add 1 @name \n >remove 1 @name \n >leader 1 \n >removeleader 1 \n >size 1 12"); 
+
+        }                    
+};
+
+
+module.exports.help =
+{
+    name: "help"
+}
