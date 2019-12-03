daaf6c2e167a4c820d6c64da9d736df2b8e3b3d6
njn-R <njn@protonmail.com>
2019-12-03 21:13:26 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports =
         execute(message)
         {         
             
-            message.channel.send(">create 1 abc \n >join 1 \n >leave 1 \n >list \n >add 1 @name \n >remove 1 @name \n >leader 1 \n >removeleader 1 \n >size 1 12"); 
+            message.channel.send(">create 1 abc \n >join 1 \n >leave 1 \n >list \n >add 1 @name \n >remove 1 @name \n >leader 1 \n >removeleader 1 \n >size 1 12 \n >details 1"); 
 
         }                    
 };
