9532debbe9ec05cd4176a431917e7386855ac1f2
njn-R <njn@protonmail.com>
2019-12-04 12:38:35 +0530

    help file

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,71 @@ module.exports =
         execute(message)
         {         
             
-            message.channel.send(">create 1 abc \n >join 1 \n >leave 1 \n >list \n >add 1 @name \n >remove 1 @name \n >leader 1 \n >removeleader 1 \n >size 1 12 \n >details 1"); 
+            const helpEmbed = {
+                color: 1752888,
+                author: {
+                    name: 'Bot Commands',
+                },
+                description: '```> = Bot prefix        # = MPA number```',
+                fields: [
+                    {
+                        name: "**>create # MPAname**",
+                        value: "Creates an MPA with given MPA name and MPA number",
+                        inline: true
+                      },
+                      {
+                        name: "**>details # **",
+                        value: "Display details of the MPA  ",
+                        inline: true
+                      },
+                      {
+                        name: "**>join #**",
+                        value: "Join the MPA",
+                        inline: true
+                      },
+                      {
+                        name: "**>leave #**",
+                        value: "Leave the MPA",
+                        inline: true
+                      },
+                      {
+                        name: "**>add # @username**",
+                        value: "Add another player to the MPA",
+                        inline: true
+                      },
+                      {
+                        name: "**>remove # @username**",
+                        value: "Remove a player from the MPA",
+                        inline: true
+                      },
+                      {
+                        name: "**>leader # @username**",
+                        value: "Assign a party leader for the MPA",
+                        inline: true
+                      },
+                      {
+                        name: "**>removeleader # @username**",
+                        value: "Remove the party leader from the MPA",
+                        inline: true
+                      },
+                      {
+                        name: "**>size # MPAsize**",
+                        value: "Change the player size of the MPA",
+                        inline: false
+                      },
+                      {
+                        name: "**>list**",
+                        value: "List all the current MPAs",
+                        inline: false
+                      },
+                ],
+                footer: {
+                    text: '"Airi MPA Bot',
+                    icon_url: 'https://i.imgur.com/HF4CEeN.png',
+                },
+            };
+            
+            message.channel.send({embed: helpEmbed}); 
 
         }                    
 };
