4c420dea92b5e06cc6d7906b425490f05605a63b
njn <njn@protonmail.com>
2020-01-03 16:00:44 +0530

    Update feed.js

--- commands/feed.js
+++ commands/feed.js
@@ -9,9 +9,9 @@ module.exports =
         let birbGifs = ["https://tenor.com/view/feed-fish-bird-throw-food-catch-gif-13838907",
                         "https://giphy.com/gifs/jVEPGJEn9gT269bDCo", "https://giphy.com/gifs/2wZ07PIbTSbenXUTxM", 
                         "https://tenor.com/view/feed-baby-bird-yum-nom-nom-gif-13782572"]
-        let sharkGifs = ["https://tenor.com/view/angry-shocked-shark-shark-attack-gif-7814441", 
-                        "https://tenor.com/view/cat-shark-kitten-surprise-gif-5053865", 
-                        "https://tenor.com/view/shark-shark-breaching-breaching-ocean-attack-gif-5104846"]
+        let sharkGifs = ["https://tenor.com/view/ocean-wow-amazing-eat-catch-gif-3462912",
+                        "https://tenor.com/view/shark-sharkbite-computer-like-lol-gif-5054888",
+                        "https://media.giphy.com/media/kJf7k5lGDQ12M/giphy.gif"]
         let tunaGifs = ["https://tenor.com/view/feeding-the-fish-feed-the-fish-fish-feed-gif-14011642", 
                         "https://giphy.com/gifs/calacademy-eating-hungry-cAgGr4wGNRxqEv5V1u", 
                         "https://tenor.com/view/fish-feed-steam-struggle-gif-7881662", 
@@ -19,10 +19,9 @@ module.exports =
         let mouseGifs = ["https://tenor.com/view/ratatouille-cute-rat-eating-spaghetti-gif-15430973", 
                         "https://tenor.com/view/pumpkin-pie-pilgrim-pie-eating-pie-happy-thanksgiving-gif-12857426", 
                         "https://tenor.com/view/mouse-flower-baby-mouse-cute-animals-gif-3544689"];
-        let nonaGifs = ["https://tenor.com/view/kitkat-trick-vegetables-food-mad-gif-3466677",
-                        "https://tenor.com/view/throw-gif-6112851",
-                        "https://tenor.com/view/hungry-throw-food-eat-this-hit-spiderman-gif-14536727", 
-                        "https://tenor.com/view/pie-face-smash-throw-food-head-shot-food-fight-gif-14729465"]
+        let nonaGifs = ["https://tenor.com/view/eat-kirby-anime-gif-4848690",
+                        "https://tenor.com/view/anime-eating-gif-8951338",
+                        "https://tenor.com/view/anime-eating-gif-9340206"]
         let rockGifs = ["https://i.imgur.com/ibeSqc8.gifv"]
         let kariGifs = ["https://tenor.com/view/cute-kitten-feed-me-gif-5668010", 
                         "https://tenor.com/view/food-eat-cat-feed-gif-12269959", 
@@ -34,12 +33,17 @@ module.exports =
                         "https://tenor.com/view/mc-donalds-anime-love-food-gif-9102220", 
                         "https://tenor.com/view/anime-foodwars-food-gif-7677145"]
         let mikoGifs = ["https://tenor.com/view/anime-feed-shy-annoyed-embarrassed-gif-9340097",
-                        "https://tenor.com/view/shamiko-eating-machikado-mazoku-yuko-yoshida-gif-14615737"]
-        let defaultGifs = ["No!", 
-                        "https://tenor.com/view/anime-steal-food-gif-10194720", 
+                        "https://tenor.com/view/shamiko-eating-machikado-mazoku-yuko-yoshida-gif-14615737",
+                        "https://tenor.com/view/eat-anime-mad-angry-gif-15485137",
+                        "https://tenor.com/view/lewd-anime-anime-eat-gif-15405723"]
+        let defaultGifs = ["https://tenor.com/view/anime-steal-food-gif-10194720", 
                         "https://tenor.com/view/so-moe-feed-food-hungry-anime-gif-8636201", 
-                        "https://tenor.com/view/kanna-kamui-gif-10168208"]
-
+                        "https://tenor.com/view/kanna-kamui-gif-10168208",
+                        "https://tenor.com/view/itsatrap-anime-trap-funny-gif-8431155", 
+                        "https://tenor.com/view/anime-food-yummy-yum-nom-gif-10281568", 
+                        "https://tenor.com/view/mc-donalds-anime-love-food-gif-9102220", 
+                        "https://tenor.com/view/anime-foodwars-food-gif-7677145"]
+        
         let user = message.mentions.users.first()
         if(args[0] === 'me')
         {
