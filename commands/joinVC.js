629dd4ed2ccd78f7b9e6f7ad96d4c3b0457237b4
njn <njn@protonmail.com>
2020-03-10 23:22:50 +0530

    Update joinVC.js

--- commands/joinVC.js
+++ commands/joinVC.js
@@ -1,18 +1,24 @@
 const commando = require('discord.js-commando')
 
-module.exports = class JoinVoiceChannelCommand extends commando.Command{
-    constructor(client){
-        super(client, {
-            name: 'joinVC',
-            group: 'music',
-            memberName: 'joinVC',
-            description: 'Joins a voice channel'
-        })
-    }
-    async execute(message) {
+// module.exports = class JoinVoiceChannelCommand extends commando.Command{
+//     constructor(client){
+//         super(client, {
+//             name: 'joinVC',
+//             group: 'music',
+//             memberName: 'joinVC',
+//             description: 'Joins a voice channel'
+//         })
+//     }
+module.exports =
+{
+    name: 'joinVC',
+    description: 'Joins a voice channel',
+    execute(message)
+    {   
+  //  async execute(message) {
         let vc = message.guild.channels.find(ch => ch.name.toLowerCase() === 'FredBot Music Room' && ch.type === 'voice')
-        if(vc && !vc.connection) {
-            await vc.join();
-        }
+        //if(vc && !vc.connection) {
+            vc.join();
+        //}
     }
 }
