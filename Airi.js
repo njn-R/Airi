0fbcc04142a871f138fa4c3337c411a16da96942
njn <njn@protonmail.com>
2020-04-12 00:10:23 +0530

    Update Airi.js

--- Airi.js
+++ Airi.js
@@ -64,22 +64,27 @@ bot.on('message', async message =>
 	else 
 	{	
 		if(message.guild.id == "597857533734027274" ) return;
-		var tags = "anime";
-		tags = tags.concat(command);
-	
-		const Tenor = require("tenorjs").client({
-			"Key": "IBEW4A0KACM3", // https://tenor.com/developer/keyregistration
-			"Filter": "off", // "off", "low", "medium", "high", not case sensitive
-			"Locale": "en_US", // Your locale here, case-sensitivity depends on input
-			"MediaFilter": "minimal", // either minimal or basic, not case sensitive
-			"DateFormat": "D/MM/YYYY - H:mm:ss A" // Change this accordingly
-		});
-		Tenor.Search.Random(tags, "1").then(Results => {
-		//Tenor.Search.Query(tags, "1").then(Results => {
-			Results.forEach(Post => {
-					message.channel.send(Post.url);
+
+		var letters = /^[A-Za-z]+$/;
+	  	if(command.value.match(letters))
+	  	{
+			var tags = "anime";
+			tags = tags.concat(command);
+		
+			const Tenor = require("tenorjs").client({
+				"Key": "IBEW4A0KACM3", // https://tenor.com/developer/keyregistration
+				"Filter": "off", // "off", "low", "medium", "high", not case sensitive
+				"Locale": "en_US", // Your locale here, case-sensitivity depends on input
+				"MediaFilter": "minimal", // either minimal or basic, not case sensitive
+				"DateFormat": "D/MM/YYYY - H:mm:ss A" // Change this accordingly
 			});
-		}).catch(console.error);
+			Tenor.Search.Random(tags, "1").then(Results => {
+			//Tenor.Search.Query(tags, "1").then(Results => {
+				Results.forEach(Post => {
+						message.channel.send(Post.url);
+				});
+			}).catch(console.error);
+		}
 	}	
 		
 });
